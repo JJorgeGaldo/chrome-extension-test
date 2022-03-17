c5e6ee134d57dfddb9c53ac35241dd99c8e1d849
Juan Jorge Galdo <juanjorgegaldo@gmail.com>
2022-03-17 09:09:48 +0100

    Updating the repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./styles.css";
+import { useState } from "react";
 import CryptoList from "./components/CryptoList";
+import "./styles.css";
 
 export default function App() {
+  const [url, setUrl] = useState('');
+
+  // Get the current Url
+  
+
   return (
     <div>
+      <h2></h2>
       <CryptoList />
     </div>
   );
