31b72ef490c0ac1ecdd264eff8852b89d9f52617
Juan Jorge Galdo <juanjorgegaldo@gmail.com>
2022-03-17 09:22:56 +0100

    The extension get the correct current tab from the browser

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CryptoList from "./components/CryptoList";
 import "./styles.css";
 
 export default function App() {
+
   const [url, setUrl] = useState('');
 
   // Get the current tab Url
-  
+  useEffect(() => {
+    const queryInfo = {active:true, lastFocusedWindow: true};
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+      const url = tabs[0].url;
+      setUrl(url);
+    });
+  }, []);
 
   return (
     <div>
-      <h2></h2>
+      <p>{url}:</p>
       <CryptoList />
     </div>
   );
